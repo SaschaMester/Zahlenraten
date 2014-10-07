3d975a605e35e73fb6fd7a57f18f1da4bda6e601
Sascha Mester <c_ur_free@yahoo.com>
2014-10-07 20:11:19 +0200

    Lizenzinformation hinzugefügt

--- zahlenraten.py
+++ zahlenraten.py
@@ -1,6 +1,29 @@
 #! /usr/bin/env python
 # -*- coding: utf-8
 
+########################################
+############# Zahlenraten ##############
+########################################
+# (c) 2014 by Sascha Mester            #
+# This program is free software.       #
+# You can freely redistribute it       #
+# under the terms of the               #
+# GNU General Public License,          #
+# version 3 or ( at your opinion )     #
+# any later version, published by      #
+# the Free Software Foundation.        #
+########################################
+# This program is published in the     #
+# hope that it will be useful,         #
+# but WITHOUT ANY WARRANTY, without    #
+# even the implied warranty of         #
+# MERCHANDIBILITY of FITNESS FOR A     #
+# PARTICULAR PURPOSE. See the          #
+# GNU GENERAL PUBLIC LICENSE for       #
+# more details.                        #
+########################################
+
+
 # Zufallsfunktion importieren
 from random import randint
 
